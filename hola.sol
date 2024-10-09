61e60de3721f46a97533d89e26e8eda1a6135a4c
stiv-ca <danistilop@gmail.com>
2024-10-08 19:16:34 -0500

    Primer Contrato

---
+++ hola.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24; //En esta parte defino la versión del compilador
+
+//Linea 1 es la licencia
+
+//A partir de aquí coloco la lógica de mi contrato
+contract hola_mundo {
+    string public saludo = "Hola desde Educaeth"; // Declaración del contenido saludo
+    
+}
